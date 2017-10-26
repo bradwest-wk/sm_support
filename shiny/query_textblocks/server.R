e594b779717a9273d34af77ccf026666be87df3d
Brad West <brad.west@workiva.com>
2017-10-25 22:08:46 -0600

    Front and back end changes

--- shiny/query_textblocks/server.R
+++ shiny/query_textblocks/server.R
@@ -3,75 +3,133 @@ library(DT)
 library(RPostgreSQL)
 library(pool)
 
+cred <- Sys.getenv(c('DB_NAME', 'DB_HOST', 'DB_PORT', 'DB_USER', 'DB_PASSWORD'))
 
+pool <- dbPool(
+  drv = RPostgreSQL::PostgreSQL(),
+  dbname = cred[1], 
+  host = cred[2],
+  port = cred[3],
+  user = cred[4],
+  password = cred[5],
+  maxSize = 7, # max size for DBI is 16
+  idleTimeout = 360000,
+  validateQuery = 'SELECT 1'
+)
 
 shinyServer(function(input, output, session) {
   
   rv <- reactiveValues(db_return = NULL)
-  
   # ====
-  # Connection and Query
+  # Query on action button
   # ====
   observeEvent(input$query, {
-
-    updateTabsetPanel(session, "inTabset", selected = 'Data')
     
-    # if (input$dbserver == "") {showNotification('Enter a Host')}
-    # if (input$dbname == "") {showNotification('Enter a Database')}
-    if (input$username == "") {showNotification('Enter a Username')}
-    if (input$password == "") {showNotification('Enter a Password')}
-    # if (input$port == "") {showNotification('Enter a Port')}
-    if (input$keywords == "") {showNotification('Enter keyword(s)')}
-   
-    # connect
-    con <- dbConnect(PostgreSQL(), host='***',
-                     port='***',
-                     user=input$username,
-                     password=input$password,
-                     dbname='***')
-    
-    req(input$keywords, input$username, input$password)
+    # notification for missing keywords
+    if (input$keywords == "") {
+      showNotification('Need at least one keyword to search for',
+                       duration = 10,
+                       closeButton = TRUE,
+                       id = 'no_keyword',
+                       type = 'error')
+    }
+    req(input$keywords)
     
     # query
-    query <- paste(
-      "SELECT * FROM _mv_textblocks
-      WHERE value ~*",
-      paste0('\'(', paste0(trimws(
-        strsplit(input$keywords, ',')[[1]]), collapse = "|"), ')\''),
-      if (input$element != "") {paste0("AND name = '", input$element, "'\n")},
-      if (input$sic != "") {paste0("AND sic = ", input$sic, "\n")},
-      if (input$cik != "") {paste0("AND cik = '", input$cik, "'\n")},
-      if (input$form_type != "") {paste0("AND form_type = '", input$form_type, 
-                                         "'\n")},
-      if (input$taxonomy != "") {paste0("AND taxonomy = '", input$taxonomy, 
-                                        "'\n")},
-      if (input$filer_status != "All") {paste0("AND filer_status = '", 
-                                            input$filer_status, "'\n")},
-      if (input$creation_software != "") {paste0("AND creation_software = '",
-                                                 input$creation_software,
-                                                 "'\n")},
-    ";"
+    keywords <-  paste0("'(", paste0(
+      trimws(strsplit(
+        input$keywords,
+        # sub("'", "\\\\'", input$keywords), 
+        ",")[[1]]), collapse = "|"), ")'")
+    
+    sql <- paste0(
+          "SELECT 
+            name,
+            company_name,
+            cik,
+            filer_status,
+            sic,
+            form_type,
+            filing_date,
+            taxonomy,
+            creation_software,
+            value
+          FROM _mv_textblocks
+          WHERE value ~* ",
+          paste0(keywords, " "),
+          if (input$element != "") {
+            paste0("AND name = '", input$element, "' ")
+            },
+          if (input$sic != "") {
+            paste0("AND sic = ", input$sic)
+            },
+          if (input$cik != "") {
+            paste0("AND cik = '", input$cik, "' ")
+            },
+          if (input$form_type != "") {
+            paste0("AND form_type = '", input$form_type, "' ")
+            },
+          if (input$taxonomy != "") {
+            paste0("AND taxonomy = '", input$taxonomy, "' ")
+            },
+          # if (input$filer_status != "All") {
+          #   paste0("AND filer_status = '", input$filer_status)
+          #   },
+          if (input$creation_software != "") {
+            paste0(" AND creation_software = '", input$creation_software, "' ")
+            },
+          if (input$limit != "") {
+            paste0(" LIMIT ", input$limit)
+          },
+          ";"
     )
-    # make sure it closes connection
-    # on.exit(dbDisconnect(con), add = TRUE)
+    
+    cat(paste('\nExecuting query:\n', sql, "\n"))
+    
+    updateTabsetPanel(session, "inTabset", selected = 'Data')
     
     # execute query
-    rv$db_return <- dbGetQuery(pool, query)
+    rv$db_return <- dbGetQuery(pool, sql)
+    
+    # log
+    cat(paste('\nThe query returned', nrow(rv$db_return),
+              'text blocks matching those keywords\n'))
+    active_connections <- dbListConnections(PostgreSQL())
+    cat(paste('\nActive Connections:', length(active_connections), "\n"))
+    
+    # handle too many connections
+    if (length(active_connections) > 15) {
+      lapply(active_connections[1:4], dbDisconnect)
+      cat("Connections deleted")
+    }
     
   })
 
   # ==== 
   # Table Output
   # ====
-  
-  
   output$results <- renderDataTable({
-    # TODO Add case when no results
     if (nrow(rv$db_return)==0) {
+      showNotification('The query executed successfully, but no text blocks 
+                       containing the keyword(s) were found.', 
+                       duration = 10,
+                       closeButton = TRUE,
+                       type = 'warning')
+      # print for log
       data.frame(Whoops = c("No results found"))
     } else {
-      datatable(rv$db_return[, c(10, 1:8)], 
-                selection = list(mode = 'single', selected = 1, target = 'row')
+      showNotification(paste('The query returned', nrow(rv$db_return),
+                             'text blocks matching those keywords'),
+                       duration = 20,
+                       closeButton = TRUE,
+                       type = 'message')
+      datatable(rv$db_return[,c('name', 'company_name', 'cik', 'filer_status',
+                                'sic', 'form_type', 'filing_date', 'taxonomy',
+                                'creation_software')],
+                options = list(scrollX = TRUE, pageLength = 5),
+                selection = list(mode = 'single', 
+                                 selected = 1, 
+                                 target = 'row')
       )
     }
   },
@@ -81,6 +139,10 @@ shinyServer(function(input, output, session) {
   # HTML Output
   # ====
   output$html_out <- renderUI({
+    validate(
+      need(!is.null(rv$db_return), 
+           message = "No textblock to show, execute a query first")
+    )
     if (!is.null(rv$db_return)) {
       HTML(rv$db_return$value[input$results_rows_selected]) 
     } else {
@@ -95,10 +157,50 @@ shinyServer(function(input, output, session) {
 # TODO: Add all documents
 # TODO: disappearing sidebar after pinning database (login button?)
 # TODO: Specify element level
-# TODO: Switch to data tab after query exectutes
-# TODO: Use isPostgresqlIdCurrent
-# TODO: Use pool to
 # TODO: Try catch for database connection issues
 # TODO: Write the query with dplyr
-# TODO: Better logs with dbGetInfo
-# TODO: Can you change it up so that when shiny opens it brings the data into memory?
+# TODO: fix multiple filer status'
+# TODO: Why does pool not close connections properly?
+# TODO: Add download button
+# TODO: Style Datatable
+
+# validate(
+#   need(try(rv$db_return <- dbGetQuery(pool, sql)),
+#        message = "That query could not be executed as is.")
+# )
+# rv$db_return <- dbGetQuery(pool, sql)
+# validate(
+#   need(!is.null(rv$db_return),
+#        message = "That query could not be executed as is. Make sure your 
+#        keywords are as desired, and contain no apostrophes")
+# )
+# get_data <- function() {
+#   df <- tryCatch(
+#     {
+#       dbGetQuery(pool, sql)
+#     },
+#     error = function(cond) {
+#       message(paste("The query failed for the following keyword(s):",
+#                            input$keywords))
+#       message("Check that your keywords do not contain apostrophes")
+#       message("Here's the original error message:")
+#       message(cond)
+#       # showNotification(paste(one, two, three, four),
+#       #                  duration = 60,
+#       #                  closeButton = TRUE,
+#       #                  type = 'error')
+#       return(NULL)
+#     },
+#     warning = function(cond) {
+#       message(paste("The query returned a warning for the following
+#                     keyword(s)", input$keywords))
+#       message("Here's the original warning message:")
+#       message(cond)
+#       return(NULL)
+#     }, 
+#     finally = print('done querying')
+#   )
+#   return(df)
+# }
+# 
+# rv$db_return <- get_data()
--- shiny/query_textblocks/ui.R
+++ shiny/query_textblocks/ui.R
@@ -1,98 +1,89 @@
 library(shiny)
 library(DT)
-
-load("./test_html.Rdata")
+library(shinysky)
 
 shinyUI(fluidPage(
   
-  # Application title
   titlePanel("Query Text Blocks"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(width = 2,
-       h3('DB Connection'),
-       # textInput(inputId = "dbserver", label = "Database Server:"),
-       # textInput(inputId = "dbname", label = "DB name:"),
-       textInput(inputId = "username", label = "DB username:"),
-       passwordInput(inputId = "password", label = "DB password:"),
-       # numericInput(inputId = "dbport", label = "DB port:", value = 8084),
-       # actionButton('connect', "Connect DB",
-       #              style = "color: #fff; background-color: #2a2dc0; border-color: #000000"),
-       actionButton('query', 'Execute Query', 
-                    style = "color: #fff; background-color: #ff0800; border-color: #000000"),
-       br(),
-       h3('Query Parameters'),
-       textInput('keywords', 
-                 label = 'Keywords - Comma Delimited/Case Insensitive'
-                 # placeholder = 'proboscis monkey, dumbo octopus'
-                 ),
-       textInput('element', 'Element (optional)', 
-                 value = NULL),
-       textInput('sic', label = 'SIC (optional)', value = NULL),
-       textInput('cik', label = 'CIK (optional)', value = NULL),
-       textInput('form_type', label = 'Form Type (optional)', 
-                 value = NULL),
-       textInput('taxonomy', label = 'Taxonomy (optional)', 
-                    value = NULL),
-       selectInput('filer_status', label = 'Filer Status (optional)', 
-                   choices = c('Large Accelerated Filer',
-                               'Accelerated Filer',
-                               'Smaller Reporting Company',
-                               'Non-accelerated Filer',
-                               'All'), selected = 'All'),
-       textInput('creation_software', label = 'Creation Software (optional)', value = NULL)
+       textInput('keywords', label = 
+                   'Keywords - Comma Delimited/Case Insensitive'),
+       textInput('limit', label = 'Max number of Rows/Use this for quicker 
+                 querying if you just want a few examples'),
+       textInput('element', 'Element (optional)'),
+       textInput('sic', label = 'SIC (optional)'),
+       textInput('cik', label = 'CIK (optional)'),
+       textInput('form_type', label = 'Form Type (optional)'),
+       textInput('taxonomy', label = 'Taxonomy (optional)'),
+       # selectInput('filer_status', label = 'Filer Status (optional)', 
+       #             choices = c('Large Accelerated Filer',
+       #                         'Accelerated Filer',
+       #                         'Smaller Reporting Company',
+       #                         'Non-accelerated Filer',
+       #                         'All'), selected = 'All'),
+       textInput('creation_software', label = 'Creation Software (optional)'),
+       shiny::actionButton('query', 'Execute Query', 
+                    style = "color: #fff; background-color: #ff0800; 
+                    border-color: #000000")
        
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
       tabsetPanel(id = "inTabset",
         tabPanel("Data",
-                 dataTableOutput('results')
-                 ),
-        tabPanel('Text Block',
+                 dataTableOutput('results'),
+                 busyIndicator(text = "Importing Data"),
+                 HTML('<hr style="height:5px;border:none;color:#2a2dc0;
+                      background-color:#2a2dc0;">'),
                  tags$div(
-                   uiOutput('html_out')
-                 )
-                ),
+                  uiOutput('html_out')
+                  )
+                 ),
+        # tabPanel('Text Block',
+        #          tags$div(
+        #            uiOutput('html_out')
+        #          ),
+        #          busyIndicator('Importing Data')
+        #         ),
         tabPanel("Instructions", value = 'instructions',
                  tags$div(
                    tags$h3("Summary"),
-                   tags$p("This app allows the user to search text blocks for certain keywords.  
-                          As of this first iteration, it only
-                          supports 2016 fiscal year 10-K filings.  This functionality and
-                          the UI will be expanded, time permitting."),
-                   tags$h3("Database Connection"),
-                   tags$p("The user must specify the connection information. Get this information
-                          from a team member, or Brad West. Currently the app does not gracefully
-                          handle incorrect information. It will throw an error and crash if the 
-                          information is not entered correctly."),
+                   tags$p("This app allows the user to search text blocks for 
+                          certain keywords. Currently only 2016 10-Ks are 
+                          accessible."),
                    tags$h3("Query Information"),
-                   tags$p("The user must specify at least one keyword to search the text blocks 
-                          for. Keywords should be comma delimited; that precludes you from 
-                          using commmas in your keywords. If this is a problem, we can change
-                          it up, just let me know. The query searches for text blocks containing any 
-                          of the keywords.  If you want blocks with at least two, or all of the 
-                          keywords, let me know."),
-                   tags$p("None of the other filtering information is needed to execute a query. 
-                          Currently the underlying data are composed
-                          only of 10-K's but this will be changed at some point or on request."),
+                   tags$p("The user must specify at least one keyword to search 
+                          the text blocks for, Comma delimit keywords, which 
+                          precludes you from using commas in the keywords. Also
+                          the app will fail if you use apostrophes. The search
+                          matches if the textblock contains any of the keywords
+                          "),
+                   tags$p("None of the other filtering information is needed to 
+                          execute a query."),
                    tags$h3("Executing Query"),
-                   tags$p("Be patient, there were a lot of footnotes in 2016 10-Ks."),
+                   tags$p("Be patient, there were a lot of footnotes in 
+                          2016 10-Ks."),
                    tags$h3("Results"),
-                   tags$p("There are two tabs, 'Data' and 'Text Block'.  The data shows the metadata
-                          of the text block.  You can sort on each column, and filter. If you click 
-                          on a row, the 'Text Block' tab is populated with the text block for that
-                          row. I know this a bit of an unweildly interface, and I'll try to change
+                   tags$p("There are two tabs, 'Data' and 'Text Block'.  
+                          The data shows the metadata of the text block.  You 
+                          can sort on each column, and filter. If you click 
+                          on a row, the 'Text Block' tab is populated with the 
+                          text block for that row. I know this a bit of an 
+                          unweildly interface, and I'll try to change
                           it into something a bit more cohesisve."),
                    tags$h3("Other"),
-                   tags$p("The html text blocks for the files should be in the level_1_html file in 
-                          the Google Drive Project Management folder. If you'd like functionality to
-                          download the html file, let me know. I'll try to add it anyway, but I might
-                          not get around to it for a while."),
-                   tags$p("As always, let me know when things go wrong and I'll try to fix them.")
-                 )
+                   tags$p("The html text blocks for the files should be in the 
+                          level_1_html file in the Google Drive Project 
+                          Management folder. If you'd like functionality to
+                          download the html file, let me know. I'll try to add 
+                          it anyway, but I might not get around to it for a 
+                          while."),
+                   tags$p("As always, let me know when things go wrong and 
+                          I'll try to fix them.")
+                 ),
+                 busyIndicator("Importing Data")
           
         ),
         selected = 'instructions'
